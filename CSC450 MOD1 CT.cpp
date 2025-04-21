6eadf915cf28aef7a1d11044541f88bb6b94627d
bsmith6csug <brian.smith6@csuglobal.edu>
2025-04-20 18:31:59 -0600

    Add files via upload

---
+++ CSC450 MOD1 CT.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+int main(){
+	std::string firstName = "Brian";
+	std::string lastName = "Smith";
+	std::string address = "2345 Road Street";
+	std::string city = "Colorado Springs";
+	std::string zipCode = "80921";
+	
+	std::cout << "First Name: " << firstName << std::endl;
+	std::cout << "Last Name: " << lastName << std::endl;
+	std::cout << "Addresss: " << address << std::endl;
+	std::cout << "City: " << city << std::endl;
+	std::cout << "Zip Code: " << zipCode << std::endl;
+	
+	return 0;
+}
---
+++ CSC450 MOD1 Corrections 1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+//Standard namespace declaration
+using namespace std;
+
+//Main Function
+int main(){
+
+     //Standard Ouput Statement
+     cout << "Welcome to this C++ Program" << endl;
+
+     cout << "I have corrected all errors for this program." << endl;
+
+     // Wait For Output Screen
+     cout << "Please press enter to continue." ;
+     cin.get();
+
+     //Main Function return Statement
+     return 0;
+
+}
